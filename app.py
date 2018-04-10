9073f23e8f7639a4b6c3899a0b3b3290be221dc4
Erin <eneaton@umichedu>
2018-04-10 09:59:38 -0400

    lecex

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import json
 import model
@@ -15,9 +15,30 @@ def index():
         </ul>
     '''
 
-@app.route('/bball')
+
+@app.route('/bball', methods=['GET', 'POST'])
 def bball():
-    return render_template("seasons.html", seasons=model.get_bball_seasons())
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_bball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_bball_seasons()
+
+    return render_template("seasons.html", sport="Men's Basketball",
+         seasons=seasons)
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+    else:
+        firstname = ''
+        lastname = ''
+    return render_template("hello.html", firstname=firstname, lastname=lastname)
 
 if __name__ == '__main__':
+    model.init_bball('umbball.csv')
     app.run(debug=True)
